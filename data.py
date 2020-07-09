3b05e1ae9a9f62ff66b0b736c6491e4422442062
nurpeiis <nb2577@nyu.edu>
2020-07-09 17:31:06 +0400

    Fix length of the sentence

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ from token_to_index import TokenToIndex
 
 class TextualDataset(Dataset):
 
-    def __init__(self, filename, token_to_index):
+    def __init__(self, filename, token_to_index, label_to_id):
         """
             filename : string = input file
             token_to_index_files : list<string> = files from which dictionary is going to be create
@@ -19,14 +19,20 @@ class TextualDataset(Dataset):
         self.classes = self.labels.unique()
         self.samples = []
         self.vocab = token_to_index.token_to_id_vocab
+        self.sent_size = 30
         for i in range(len(tokenized_sentences)):
             indexed_sentence = [n.strip()
                                 for n in ast.literal_eval(tokenized_sentences[i])]
             for j in range(len(indexed_sentence)):
                 indexed_sentence[j] = token_to_index.token_to_id(
                     indexed_sentence[j])
+            while len(indexed_sentence) < self.sent_size:
+                indexed_sentence.append(
+                    token_to_index.token_to_id_vocab['<PAD>'])
+            if len(indexed_sentence) > self.sent_size:
+                indexed_sentence = indexed_sentence[:30]
             self.samples.append(
-                (indexed_sentence, self.labels[i]))
+                (indexed_sentence, label_to_id[self.labels[i]]))
 
     def __len__(self):
         return len(self.samples)
@@ -34,5 +40,6 @@ class TextualDataset(Dataset):
     def __getitem__(self, idx):
         indexed_sentence = torch.tensor(
             self.samples[idx][0], dtype=torch.long)
-        label = self.samples[idx][1]
+        label = torch.tensor(
+            self.samples[idx][1], dtype=torch.long)
         return indexed_sentence, label
