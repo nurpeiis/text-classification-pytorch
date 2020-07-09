09b3d926695cd385445af94c20c5bd536e942831
nurpeiis <nb2577@nyu.edu>
2020-07-09 10:15:59 +0400

    Change token_to_index to be able to get list represented as string into list

--- token_to_index.py
+++ token_to_index.py
@@ -2,20 +2,26 @@ import csv
 import ast
 import pandas as pd
 
+PAD_TOKEN = '<PAD>'
+UNK_TOKEN = '<UNK>'
+
 
 class TokenToIndex:
 
     def __init__(self, input_files, output_dict):
-        """ 
+        """
             input_files : list<string> = list of files from which the sentences is going to be extracted
             output_dict : string = name of the file where token to index will be saved
         """
         self.input_files = input_files
         self.output_dict = output_dict
+        self.token_to_id = dict()
 
     def get_dict(self):
         words = dict()
-        counter = 0
+        counter = 2
+        words[PAD_TOKEN] = 0
+        words[UNK_TOKEN] = 1
         for file in self.input_files:
             df = pd.read_csv(file, delimiter='\t', header=0)
             tmp_word_list = df['tokenized']
@@ -30,3 +36,13 @@ class TokenToIndex:
         output = csv.writer(open(self.output_dict, "w"))
         for key, val in words.items():
             output.writerow([key, val])
+        self.token_to_id = words
+
+    def token_to_id(self, token):
+        if token in self.token_to_id:
+            return self.token_to_id[token]
+        else:
+            return self.token_to_id[UNK_TOKEN]
+
+    def __len__(self):
+        self.token_to_id
